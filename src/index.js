89f775f2d3b82daa1c0c61cf3fbb1890e2394753
HoangQuangThang51900774 <51900774@student.tdtu.edu.vn>
2023-03-18 13:27:17 +0700

    MVC: createroute, controller

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ const path =require('path')
 const app = express()
 const port = 8080
 
+//chỉ cần gõ tên thư mục thì file index sẽ đc nạp nếu có
+const route=require('./routes')
+
 app.use(express.static(path.join(__dirname,'public')))
+app.use(express.urlencoded({
+  extended:true
+}))//xử lý data của form gởi lên server-post
+app.use(express.json())// xử lý data post lên bằng code-script
 
 //HTTP logger
-app.use(morgan('combined'))
+//app.use(morgan('combined'))
 
 //template engine- sử dụng engine là handlebars với thư viện handlebar khai báo ở trên
 app.engine('hbs', handlebars.engine({
@@ -17,20 +24,9 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views',path.join(__dirname,'resources/views'))
 
-//arrow function
-app.get('/', (req, res) => {
-  res.render('home');
-})
+//route init
+route(app);
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
-
-/* function bình thường
-app.get('/', function(req, res){
-  return res.send('Hello Worlsd!')
-})
-*/
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
---
+++ src/routes/index.js
@@ -0,0 +1,34 @@
+const newsRouter = require('./news')
+const siteRouter = require('./site')
+function route(app){
+    
+    
+    app.use('/news',newsRouter)
+    app.use('/',siteRouter)
+
+    //arrow function
+    // app.get('/', (req, res) => {
+    //     res.render('home');
+    // })
+    /* function bình thường
+    app.get('/', function(req, res){
+    return res.send('Hello Worlsd!')
+    })
+    */
+
+    // app.get('/news', (req, res) => {    
+    //     console.log(req.query.q)
+    //     res.render('news');
+    // })
+    
+    // app.get('/search', (req, res) => {
+    //     res.render('search');
+    // })
+    
+    // app.post('/search', (req, res) => {
+    //     console.log(req.body)
+    //     res.send('');
+    // })
+}
+
+module.exports=route;
